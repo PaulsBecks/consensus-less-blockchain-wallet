e49202a30b301a786efc0800d52bd7d6aa562691
PaulsBecks <paul.beck.berlin@gmail.com>
2020-03-04 20:16:46 +0100

    Updates

---
+++ precache-manifest.71db9ec167bf8199ecb6952183eb865c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4962cead20b26e4930ed7cbbd74f9b17",
+    "url": "/consensus-less-blockchain-wallet/index.html"
+  },
+  {
+    "revision": "11cb29429eb845ab000d",
+    "url": "/consensus-less-blockchain-wallet/static/css/main.6a828a52.chunk.css"
+  },
+  {
+    "revision": "c30fd4438b588c359614",
+    "url": "/consensus-less-blockchain-wallet/static/js/2.2dd70f66.chunk.js"
+  },
+  {
+    "revision": "4a0154fcf891bb7d01459080b9651b9f",
+    "url": "/consensus-less-blockchain-wallet/static/js/2.2dd70f66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11cb29429eb845ab000d",
+    "url": "/consensus-less-blockchain-wallet/static/js/main.2b24fd6d.chunk.js"
+  },
+  {
+    "revision": "52c9d7e43d88f2c97b71",
+    "url": "/consensus-less-blockchain-wallet/static/js/runtime-main.950a62b3.js"
+  }
+]);
